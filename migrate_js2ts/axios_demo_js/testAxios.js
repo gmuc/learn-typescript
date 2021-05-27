9725976e0ba23eca245cd4f5787466aa44d2ce12
Gerd Mucha <Gerd.Mucha@br.de>
2021-05-27 16:19:53 +0200

    first version migrate_js2ts

---
+++ migrate_js2ts/axios_demo_js/testAxios.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+
+const url = "https://swapi.dev/api/people/4";
+
+const fetchData = async (url, dataManager, encoding = "utf8") => {
+  await axios
+    .request({
+      method: "GET",
+      url: url,
+      responseType: "arraybuffer",
+      responseEncoding: "binary",
+    })
+    .then(async (response) => {
+      // or latin1, utf8
+      let data = response.data.toString(encoding);
+      dataManager(data, response);
+    })
+    .catch((error) => {
+      if (error.code == "ECONNREFUSED") {
+        dataManager(`Error: ECONNREFUSED URL: ${error.config.url}`);
+      } else {
+        console.log(error);
+      }
+    });
+};
+
+const swapiShowData = (data) => {
+  console.log("swapi:", data);
+};
+
+fetchData(url, swapiShowData);
